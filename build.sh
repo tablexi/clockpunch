3bb1b3fee8f7db5467b60034e1831ef9418e2ac4
Jason Hanggi <jason@tablexi.com>
2013-07-01 14:43:13 -0500

    moving output files into app/assets for gem building

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-coffee -cj "bin/time-parsing.js" coffee/*.coffee
-sass styles/tooltip.scss bin/tooltip.css
+coffee -cj "app/assets/javascripts/time-parsing.js" coffee/*.coffee
+sass styles/tooltip.scss app/assets/stylesheets/tooltip.css
